96c81c591415b13f523da68e6692bdad850caab8
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2023-10-16 22:30:34 +0530

    update javascript file

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /*=================Typing animation ===================*/
 var typed=new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Grapic Designer", "YouTuber"],
+    strings:["","Flutter Developer","Web Developer","Grapic Designer", "Web Designer","Project Manager"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
